1213de57c1cd433ca5f3a81356f1d8124e3dc9dc
Makar Rusanivsky <mkr.rus83@gmail.com>
2024-12-21 02:42:10 +0200

    bug fix in file goods_tags.py

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,8 +1,8 @@
 from django import template
-from goods.models import Category
+from goods.models import Categories
 
 register = template.Library()
 
 @register.simple_tag
 def tag_categories():
-    return Category.objects.all()
+    return Categories.objects.all()
