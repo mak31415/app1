bec7e4856f6f7f0894abf3bd14518fd036c5a10e
Makar Rusanivsky <mkr.rus83@gmail.com>
2024-12-22 01:00:54 +0200

    Slug converter for categories

--- goods/urls.py
+++ goods/urls.py
@@ -5,7 +5,7 @@ from goods.views import catalog, product
 app_name = "goods"
 
 urlpatterns = [
-    path('', catalog, name='index'), 
+    path('<slug:category_slug>', catalog, name='index'), 
     #path('product/', product, name='product'),
     path('product/<int:product_id>/', product, name='product'),
     path('product/<slug:product_slug>/', product, name='product'),
