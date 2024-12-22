bbd59ab458cbf8414b5dcefcbb4c34339d812ac5
Makar Rusanivsky <mkr.rus83@gmail.com>
2024-12-22 02:09:36 +0200

    The first pagination option

--- goods/urls.py
+++ goods/urls.py
@@ -6,7 +6,8 @@ app_name = "goods"
 
 urlpatterns = [
     path('<slug:category_slug>', catalog, name='index'), 
-    #path('product/', product, name='product'),
+    path('<slug:category_slug>/<int:page>/', catalog, name='index'),
+    #path('product/', product, name='product'), # Перенаправление на страницу с каталогом
     path('product/<int:product_id>/', product, name='product'),
     path('product/<slug:product_slug>/', product, name='product'),
 ]
