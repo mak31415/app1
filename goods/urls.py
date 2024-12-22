107388f8f03fd97dcbbf6ea3c14f337c30a9f254
Makar Rusanivsky <mkr.rus83@gmail.com>
2024-12-23 01:48:45 +0200

    Simple search created and add categories and prod json

--- goods/urls.py
+++ goods/urls.py
@@ -5,9 +5,10 @@ from goods.views import catalog, product
 app_name = "goods"
 
 urlpatterns = [
+    path('search/', catalog, name='search'),
     path('<slug:category_slug>', catalog, name='index'), 
     # path('<slug:category_slug>/<int:page>/', catalog, name='index'), 
     # path('product/', product, name='product'), # Перенаправление на страницу с каталогом
-    path('product/<int:product_id>/', product, name='product'),
+    # path('product/<int:product_id>/', product, name='product'),
     path('product/<slug:product_slug>/', product, name='product'),
 ]
