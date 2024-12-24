87f0340cc32b6a9a6a9d18f63a9da0ebd74be4fa
Makar Rusanivsky <mkr.rus83@gmail.com>
2024-12-25 00:23:57 +0200

    Login form and controlles are ready

--- users/views.py
+++ users/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
+from .forms import UserLoginForm
+from django.contrib import auth, messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def login(request):
+  if request.method == 'POST':
+    form = UserLoginForm(data=request.POST)
+    if form.is_valid():
+      username = request.POST['username']
+      password = request.POST['password']
+      user = auth.authenticate(username=username, password=password)
+      
+      if user:
+        auth.login(request, user)
+        return HttpResponseRedirect(reverse('main:index'))
+  else:
+    form = UserLoginForm()
+
+      
   context = {
     'title': 'Home - Авторизация',
+    'form': form,
   }
   return render(request, 'users/login.html', context)
 
