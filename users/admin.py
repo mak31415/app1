9abc2f3e0dc78b0302248766c4aad535f6dea614
Makar Rusanivsky <mkr.rus83@gmail.com>
2024-12-27 23:00:39 +0200

    Admin panel setup

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
+from carts.admin import CartTabAdmin
 
 from users.models import User
 
-admin.site.register(User)
+# admin.site.register(User)
 
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_active')
+    list_editable = ('is_staff', 'is_active')
+    search_fields = ('username', 'email')
+    list_filter = ('is_staff', 'is_active')
+    fields = (
+        'username',
+        'email',
+        'is_staff',
+        'is_active',
+    )
 
+    inlines = [CartTabAdmin,]
