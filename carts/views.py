304de366b672cb70b92753be7cb242b45cd48b49
Makar Rusanivsky <mkr.rus83@gmail.com>
2024-12-27 02:36:22 +0200

    Add-to-cart ajax request and json responce

--- carts/views.py
+++ carts/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from .models import Cart
+from django.template.loader import render_to_string
 from goods.models import Products
+from carts.utils import get_user_carts
 
-def cart_add(request, product_slug):
+def cart_add(request):
     
-    product = Products.objects.get(slug=product_slug)
+    product_id = request.POST.get('product_id')
+    product = Products.objects.get(id=product_id)
 
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
@@ -15,8 +19,18 @@ def cart_add(request, product_slug):
             cart.save()
         else:
             cart = Cart.objects.create(user=request.user, product=product, quantity=1)
+    
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        'carts/includes/included_cart.html', {"carts": user_cart}, request=request
+        )
+    
+    responce_data = {
+        'message': 'Товар добавлен в корзину',
+        'cart_items_html': cart_items_html,
+    }
 
-    return redirect(request.META.get('HTTP_REFERER'))
+    return JsonResponse(responce_data)
 
 def cart_change(request, product_slug):
     ...
